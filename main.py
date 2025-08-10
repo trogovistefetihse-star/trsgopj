710423fa09e2dcdfd864ccc749b549906f67978f
trogovistefetihse-star <trogovistefetihse@gmail.com>
2025-08-10 23:20:56 +0600

    Update main.py

--- main.py
+++ main.py
@@ -207,4 +207,5 @@ def uptime():
     }
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Render sets PORT to 10000
+    app.run(host='0.0.0.0', port=port, debug=True)
