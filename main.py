6baf8d878a7f28dbeee0e86b66f74d5989324880
trogovistefetihse-star <trogovistefetihse@gmail.com>
2025-08-10 16:09:31 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -209,4 +209,5 @@ def uptime():
     }
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port
+    app.run(host="0.0.0.0", port=port)
